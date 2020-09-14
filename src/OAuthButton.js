7c0a5e13aca46a5b0992af09181469d49526294b
Karthik P <karthik.p@publicissapient.com>
2020-09-14 11:24:13 +0530

    updated aws_exports-2

--- src/OAuthButton.js
+++ src/OAuthButton.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 class OAuthButton extends React.Component {
   handleClick() {
     // do something meaningful, Promises, if/else, whatever, and then
-    window.location.assign('https://amplify-app.auth.eu-west-1.amazoncognito.com/oauth2/authorize?identity_provider=AzureAD&redirect_uri=https://master.dj464q5nsb6u9.amplifyapp.com/&response_type=TOKEN&client_id=tp05f01r71u8666c0jimnj779');
+    window.location.assign('https://ampkarthi.auth.eu-west-1.amazoncognito.com/oauth2/authorize?identity_provider=AzureAD&redirect_uri=https://master.dj464q5nsb6u9.amplifyapp.com/&response_type=TOKEN&client_id=4gd8fs90pmgr5fvlb48u996dsg');
   }
 
   render() {
